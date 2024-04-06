039c29c13a013a953b31cb50383bb72bf80af347
JBeckintosh <James-beck@live.com>
2024-04-07 11:19:09 +1200

    Updated links for player-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import { PlayerFormComponent } from './player-form/player-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, PlayerFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
