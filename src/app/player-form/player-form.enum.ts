cc137d6ec85996e547ef4c97bfd55649e6c8a30a
JBeckintosh <James-beck@live.com>
2024-04-07 11:18:47 +1200

    Customer validator messages

---
+++ src/app/player-form/player-form.enum.ts
@@ -0,0 +1,5 @@
+export const validators = {
+    PLAYER_NAME_REQUIRED: 'Player name is required',
+    MEAL_COST_REQURIED: 'Meal cost is required',
+    MEAL_COST_MIN_VALUE: 'Meal cost cannot be less than 0.01'
+};
