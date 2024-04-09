e7e48dffbf1f60b978f9ec72bb2f13bd54f33987
JBeckintosh <James-beck@live.com>
2024-04-09 12:23:38 +1200

    Adding other enums used in the component and test suite

--- src/app/player-form/player-form.enum.ts
+++ src/app/player-form/player-form.enum.ts
@@ -2,4 +2,14 @@ export const validators = {
     PLAYER_NAME_REQUIRED: 'Player name is required',
     MEAL_COST_REQURIED: 'Meal cost is required',
     MEAL_COST_MIN_VALUE: 'Meal cost cannot be less than 0.01'
-};
+}
+
+export const validatorErrorTypes = {
+    REQUIRED: 'required',
+    MINIMUM: 'min'
+}
+
+export const formControlNames = {
+    PLAYER_NAME: 'playerName',
+    MEAL_COST: 'mealCost'
+}
