d8124e9382a05b208c098f8141dc71e386257c8a
JBeckintosh <James-beck@live.com>
2024-10-17 22:10:54 +0100

    Adding Players Table reference to the app component

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PlayerFormComponent } from './player-form/player-form.component';
+import { PlayersTableComponent } from './players-table/players-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PlayerFormComponent],
+  imports: [RouterOutlet, PlayerFormComponent, PlayersTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
